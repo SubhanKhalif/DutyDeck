684c93803a596d990addbcd5668181a65fbdc9f3
SUBHAN KHALIF <124420243+SubhanKhalif@users.noreply.github.com>
2025-07-24 12:30:22 +0530

    Update api.js

--- task-management-system/src/api.js
+++ task-management-system/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+// fallback to localhost:5000 if env variable is not defined
 const API = axios.create({
-  baseURL: "https://dutydeck-1.onrender.com/api", //import.meta.env.VITE_API_BASE_URL ||
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
 });
 
 export default API;
